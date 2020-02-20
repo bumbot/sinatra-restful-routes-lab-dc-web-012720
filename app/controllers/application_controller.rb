cec56ffe067e7266a5e58dbe6d2d67d1082755b8
bumbot <pkim1295@gmail.com>
2020-02-19 21:14:36 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,49 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  #read data
+  get '/recipes' do
+    Recipe.find_or_create_by(name: "Fried Rice", ingredients: "Rice, Meat, Vegetables, Oil", cook_time: 10)
+    erb :index
+  end
+
+  #
+  #create new recipe
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.find_or_create_by(name: params["name"], ingredients: params["ingredients"], cook_time: params["cook_time"])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  #
+  #update existing recipe
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_or_create_by(params[:id])
+    @recipe.update(name: params["name"], ingredients: params["ingredients"], cook_time: params["cook_time"])
+    erb :show
+  end
+
+  #
+  #delete recipe
+
+  delete '/recipes/:id' do
+    Recipe.find(params[:id]).delete
+    redirect "/recipes"
+  end
+
+  #wildcard--show
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :show
+  end
+
 end
